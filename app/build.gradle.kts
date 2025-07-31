a3c0a544c8cbd8b634734ad6956ad3a68d35577c
axwxs <emailalirashid@gmail.com>
2025-07-31 08:08:07 -0700

    Updating min SDK & Adding APK

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -8,7 +8,7 @@ android {
 
     defaultConfig {
         applicationId = "com.example.loginapp"
-        minSdk = 28
+        minSdk = 29
         targetSdk = 36
         versionCode = 1
         versionName = "1.0"
